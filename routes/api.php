bf26504014dbeed978bb8ba1ad83f16468608ab7
alden <connect@alenev.name>
2022-11-25 21:22:36 +0200

    add Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('stuff', '\App\Http\Controllers\api\StuffController@getStuff');
 });
